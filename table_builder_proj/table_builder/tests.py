da34668c13ce3d90b3925ec842c2744b5be3c2ce
Althea Smith <althea.o.smith@gmail.com>
2015-12-16 17:06:37 -0500

    added and ran tests

--- table_builder_proj/table_builder/tests.py
+++ table_builder_proj/table_builder/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-from table-builder.models import Account
-# Create your tests here.
+from table_builder.models import Account
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        Account.objects.create(
+            name="BoA",
+            amount=20,
+            status='Inactive'
+        )
+        Account.objects.create(
+            name="CitiBank",
+            amount=250,
+            status='Active'
+        )
+
+    def test_account_info_stored_in_db(self):
+        BoA = Account.objects.get(name="BoA")
+        CitiBank = Account.objects.get(name="CitiBank")
+        self.assertEqual(BoA.amount, 20)
+        self.assertEqual(CitiBank.status, 'Active')
+
+    def tearDown(self):
+        Account.objects.get(name="BoA").delete()
+        Account.objects.get(name="CitiBank").delete()
