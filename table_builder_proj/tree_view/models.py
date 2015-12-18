7341a25da3230d75f8500e244d4b0f2f91cc09cf
Althea Smith <althea.o.smith@gmail.com>
2015-12-18 03:14:50 -0500

    working function pulling data from database

--- table_builder_proj/tree_view/models.py
+++ table_builder_proj/tree_view/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 class TreeNode(models.Model):
     text = models.CharField(max_length=50)
-    icon = models.CharField(max_length=50, null=True, blank=True)
-    selectedIcon = models.CharField(max_length=50, null=True, blank=True)
-    color = models.CharField(max_length=15, null=True, blank=True)
-    backColor = models.CharField(max_length=15, null=True, blank=True)
-    href = models.CharField(max_length=15, null=True, blank=True)
-    selectable = models.NullBooleanField()
     parentNode = models.ForeignKey("TreeNode", null=True, blank=True)
 
     def __str__(self):
--- table_builder_proj/tree_view/static/tree_view/tree_view.js
+++ table_builder_proj/tree_view/static/tree_view/tree_view.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  // Changed 'label' to 'text' and 'items' to 'nodes'
+  // Changed 'label' to 'text' and 'items' to 'nodes' for bootstrap-treeview
   var items = {
       text: 'Node Label',
       nodes: [
@@ -31,20 +31,7 @@ $(document).ready( function() {
       ]
   };
 
-  // Function to adjust keys
-  // var items = {
-  //     label: 'Node Label',
-  //     items: = []
-  //   }
-  //
-  // function changeKeys(items) {
-  //   for (key, val) in items {
-  //     if key == 'label':
-  //   }
-  // }
-
-
-
+  // Adding data with static data above
   function getTree(items) {
     // treeview takes dictionary for arguments which requires elements to be in
     // an array of dictionaries
@@ -56,8 +43,62 @@ $(document).ready( function() {
     }
   };
 
-  // Function to build tree
-  $('#tree').treeview({data: getTree(items)});
-  $('#tree2').treeview({data: getTree(tree)});
+  // Helper Function for AJAX call
+  function findChildNodes(data, node) {
+    for (i in data) {
+      if (data[i].parentNode == node.id) {
+        node['node_ids'].push(data[i].id);
+      };
+    };
+    return node;
+  };
+
+  // Recursive function for finding parent nodes
+  function findParent(tree_data, node) {
+    for (i in tree_data) {
+      if ($.inArray(node.id, tree_data[i].node_ids) != -1) {
+        tree_data[i].nodes.push(node);
+        nodes_added.push(node);
+        console.log(node.parentNode, 'added to', tree_data[i]);
+        console.log('nodes added', nodes_added);
+        if (tree_data[i].node_ids.length == tree_data[i].nodes.length) {
+          delete tree_data[i].node_ids;
+        }
+      } else if (tree_data[i].nodes.length > 0) {
+        findParent(tree_data[i].nodes, node);
+      }
+    }
+    return nodes_added
+  }
+
+  //AJAX call to database to return data
+  $.get('/api/v1/TreeNode', function(data) {
+    var tree_data = [];
+    var nodes_searched = [];
+    nodes_added = []
+
+    for (i in data) {
+      data[i]['nodes'] = []
+      data[i]['node_ids'] = []
+      nodes_searched.push(findChildNodes(data, data[i]));
+      count = nodes_searched.length;
+    }
+
+    for (i in data) {
+      if (data[i].parentNode == null) {
+        tree_data.push(data[i]);
+        console.log(data[i].text, 'added');
+        nodes_added.push(data[i]);
+      }
+      else {
+        nodes_added = findParent(tree_data, data[i]);
+      }
+    }
+    console.log(tree_data)
+    $('#tree').treeview({data: getTree(tree_data)});
+  });
+
+  // Function to build tree without AJAX/API
+  // $('#tree').treeview({data: getTree(items)});
 
 });
